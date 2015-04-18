351a40eaae3e2cc0139ddda19d13a01a1a6eb5d0
Sid <sidharth@knewton.com>
2015-04-18 15:18:47 -0400

    Skeleton of the infrastructure
    1. The tox file that can run and shutdown the server
    2. The templates and the static js, css, fonts stuff hooked in together.

--- bookz/app.py
+++ bookz/app.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__) #, template_folder='bookz/templates')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def login():
+    return render_template('index.html', static_url_path='')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+	shutdown_server()
+	return 'Server shutting down...'
+
+def start_server():
+    app.run(debug=True)
+
+def shutdown_server():
+	func = request.environ.get('werkzeug.server.shutdown')
+	if func is None:
+		raise RuntimeError('Not running with the Werkzeug Server??')
+	func()
+
+# TODO: get the server port and append it here...
+def stop_server():
+	try:
+		requests.post(
+			'http://127.0.0.1:5000/shutdown')
+	except requests.exceptions.ConnectionError as e:
+		print 'Not detecting a started server..'
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "an_example_pypi_project",
+    version = "0.0.1-SNAPSHOT",
+    author = "Namrata Mendon",
+    author_email = "mendon.namrata@gmail.com",
+    description = (" A class project"),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    packages=['bookz', 'bookz.tests'],
+    long_description=read('README'),
+    entry_points = {
+		'console_scripts':[
+			'start_server =  bookz.app:start_server',
+			'stop_server = bookz.app:stop_server'
+		]
+	},
+
+	# Helps include the templates
+	# Use bower for the js and css component install
+	package_data={'bookz': ['templates/*.html']},
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
